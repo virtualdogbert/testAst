45f86199dd72e72ffd27d9f541d22703687f8ed7
Software Engineering  Geek <virtualdogbert@gmail.com>
2015-09-19 17:39:04 -0400

    Adding a dummy domain to test with

---
+++ grails-app/domain/Sprocket.groovy
@@ -0,0 +1,13 @@
+/**
+ * Just a test domain
+ */
+class Sprocket {
+    String material
+    Date   dateCreated
+    Date   lastUpdated
+
+    static constraints = {
+        dateCreated nullable: true
+        lastUpdated nullable: true
+    }
+}
